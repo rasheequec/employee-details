fec4c4019317b41d829af2e6a8c8aee3d2e7f4c9
Rasheeque <rasheequec@gmail.com>
2020-09-20 20:30:07 +0530

    client build file

---
+++ client/build/precache-manifest.84853712c91a8c08fae00c5acd0fe487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6b3cb2d77b77d0314a6d0245da73f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "287041bcc3be22ebb787",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e52b056b48a33b009648",
+    "url": "/static/css/main.e853f820.chunk.css"
+  },
+  {
+    "revision": "287041bcc3be22ebb787",
+    "url": "/static/js/2.9d381e53.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9d381e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52b056b48a33b009648",
+    "url": "/static/js/main.9ebf8224.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
